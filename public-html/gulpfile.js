72183f3f2abaefb8f2e279a997e8050cffd8c6d0
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-10-13 11:41:47 +0100

    Amended my website template

--- public-html/gulpfile.js
+++ public-html/gulpfile.js
@@ -11,14 +11,17 @@ gulp.task('copy', function() {
       '!**/bootstrap-theme.*',
       '!**/*.map'
     ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+    .pipe(gulp.dest('vendor/bootstrap'));
+
+    gulp.src(['node_modules/bootstrap-social/assets/**/*'])
+        .pipe(gulp.dest('vendor/bootstrap-social'));
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
+    .pipe(gulp.dest('vendor/jquery'));
 
   gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(gulp.dest('vendor/popper'))
-})
+});
 
 // Default task
 gulp.task('default', ['copy']);
@@ -30,7 +33,7 @@ gulp.task('browserSync', function() {
       baseDir: ''
     },
   })
-})
+});
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync'], function() {
